e7e879385f5ad8447414952b8427c705af8416df
hereiam123 <bdemaio1@gmail.com>
2018-07-12 22:31:25 -0400

    Mapped dispatch to search bar, and added Weather API promise data city to state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
 const API_KEY = '3c133af70264ac068398f0d33a19c8b5';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`;
-  const response = axios.get(url);
+  const request = axios.get(url);
 
+  console.log('Request:',request);
   return {
     type: FETCH_WEATHER,
-    payload: response
+    payload: request
   };
 }
