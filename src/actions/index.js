127deef1ed56ba30a805c6e6bdf17cff1bf2ff55
hereiam123 <bdemaio1@gmail.com>
2018-07-12 21:32:47 -0400

    added API call to Forecast Weather

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '3c133af70264ac068398f0d33a19c8b5';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const response = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: response
+  };
+}
